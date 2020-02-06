b92e4c1ff775759868275576aa3ba01dfedbced6
Adam G Horn <hornadam8@gmail.com>
2020-02-06 21:00:45 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -15,8 +15,8 @@ class Author
     @@post_count += 1
   end
   
-  def add_post_by_name(name)
-    post = Post.new(name)
+  def add_post_by_title(title)
+    post = Post.new(title)
     self.posts << post
     post.author = self
     @@post_count +=1
