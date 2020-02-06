d3c70ca9dd14c5e6a35c3b993d342fa2ad2ab1ea
Adam G Horn <hornadam8@gmail.com>
2020-02-06 20:50:31 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -2,12 +2,11 @@ class Artist
   
   attr_accessor :name
   
-  @@song_count = 0
+  @@song_count = 1
   
   def initialize(name)
     @name = name
     @songs = []
-    @@song_count += 1
   end
   
   def add_song(song)
